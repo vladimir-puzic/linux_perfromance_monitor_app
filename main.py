92ca4106f5b6fa44c96c1701efa923fb185ca6c7
Vladimir Puzic <vladimir.puzic@gmail.com>
2024-11-10 14:41:10 +0100

    fix: adjusted virtual_memory() unpacking as the returned tuple is different for windows and linux systems

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import psutil
 cpu = psutil.cpu_percent(interval=1)
-mem_total, mem_available, mem_percent, mem_used, mem_free = psutil.virtual_memory()
+mem = psutil.virtual_memory()
+mem_percent = mem[2]
+mem_used = mem[3]
+mem_total = mem[0]
 
 print(f'CPU: {cpu}%')
 print(f'RAM: {mem_percent}% - {(mem_used / 1000000000):.2f} / {(mem_total / 1000000000):.2f}')

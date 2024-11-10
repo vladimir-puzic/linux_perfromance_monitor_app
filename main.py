40732dea28d236c02c2ac4eb1b6bb3fa6c5e3c53
Vladimir Puzic <vladimir.puzic@gmail.com>
2024-11-10 14:33:34 +0100

    feat: added cpu load and memiry usage monitoring capability

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-print ('Hello world!')
+import psutil
+cpu = psutil.cpu_percent(interval=1)
+mem_total, mem_available, mem_percent, mem_used, mem_free = psutil.virtual_memory()
+
+print(f'CPU: {cpu}%')
+print(f'RAM: {mem_percent}% - {(mem_used / 1000000000):.2f} / {(mem_total / 1000000000):.2f}')
